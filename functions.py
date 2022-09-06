8d6214840ef354d950aa40f3ddf41385f6ca8a21
oguinjr <frankoguinjr@gmail.com>
2022-09-05 17:13:33 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
-D = "##TODO_FIX_ME.csv##"
+D = "supply.csv"
 coeffs = {'TALL_CAN_VOL': 0.1238483, 'SHORT_CAN_VOL': 0.09007152, 'BRICK_VOL': 3.5}
 
 
@@ -71,7 +71,7 @@ class Water:
                 return closest_date_obj, line['medium'], line['medium_num'], closest_days
 
     def rewrite_file(self, new_file):
-        with open("##TODO_FIX_ME.csv##", 'w') as csv_file:
+        with open("supply.csv", 'w') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=["type", "medium", "medium_num", "expiration"])
             writer.writeheader()
             writer.writerows(new_file)
