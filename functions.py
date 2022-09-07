21f3bb26c737d07ff9ac6045db7d3ba1cbe1a1b8
oguinjr <frankoguinjr@gmail.com>
2022-09-06 21:36:30 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,18 +5,20 @@ from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
-D = "supply.csv"
-coeffs = {'TALL_CAN_VOL': 0.1238483, 'SHORT_CAN_VOL': 0.09007152, 'BRICK_VOL': 3.5}
-
 
 class Water:
+    VOLUMES = {'TALL_CAN_VOL': 0.1238483, 'SHORT_CAN_VOL': 0.09007152, 'BRICK_VOL': 3.5}
+
     def __init__(self):
         self.dic_list = []
-        with open("supply.csv", 'r', newline='') as csv_file:
+        with open("/workspaces/22947782/project/supply.csv", 'r', newline='') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for line in csv_reader:
                 self.dic_list.append({"type": line["type"], "medium": line["medium"], "medium_num": line["medium_num"], "expiration": line["expiration"]})
 
+
+
+    # Getter
     def get_dic_list(self):
         return self.dic_list
 
@@ -24,12 +26,13 @@ class Water:
         total = 0
         for entry in self.get_dic_list():
             if entry['medium'] == "tall_can":
-                total += float(int(entry["medium_num"]) * coeffs["TALL_CAN_VOL"])
+                total += float(int(entry["medium_num"]) * self.VOLUMES["TALL_CAN_VOL"])
             elif entry['medium'] == "short_can":
-                total += float(int(entry["medium_num"]) * coeffs["SHORT_CAN_VOL"])
+                total += float(int(entry["medium_num"]) * self.VOLUMES["SHORT_CAN_VOL"])
             elif entry['medium'] == "water_brick":
-                total += float(int(entry["medium_num"]) * coeffs["BRICK_VOL"])
+                total += float(int(entry["medium_num"]) * self.VOLUMES["BRICK_VOL"])
         return round(total)
+
 
     def remove(self, match, rmv):
         dic_list = self.get_dic_list()
@@ -53,7 +56,7 @@ class Water:
     ################### THIS DOES NOT ALLOW FOR MULTIPLE DATES THAT ARE THE SAME ######################
     def next_exp(self):
         # create a list of expiration dates from csv file
-        exp_list = list(map((lambda x: x['expiration']), self.get_dic_list()))
+        exp_list = [x['expiration'] for x in self.get_dic_list()]
         today = (datetime.now()).date()
         delta_list = []
 
@@ -71,7 +74,7 @@ class Water:
                 return closest_date_obj, line['medium'], line['medium_num'], closest_days
 
     def rewrite_file(self, new_file):
-        with open("supply.csv", 'w') as csv_file:
+        with open("/workspaces/22947782/project/supply.csv", 'w') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=["type", "medium", "medium_num", "expiration"])
             writer.writeheader()
             writer.writerows(new_file)
