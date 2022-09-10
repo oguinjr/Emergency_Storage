2cb1d7fb4c2bd4b8b7d65083bfd21a31527957a4
oguinjr <frankoguinjr@gmail.com>
2022-09-09 18:03:49 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,14 +8,13 @@ from datetime import timedelta
 
 class Water:
     VOLUMES = {'TALL_CAN_VOL': 0.1238483, 'SHORT_CAN_VOL': 0.09007152, 'BRICK_VOL': 3.5}
-
     def __init__(self, filename="/workspaces/22947782/project/supply.csv"):
         self.supply_list = []
         self.filename = filename
         with open(filename, 'r', newline='') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for line in csv_reader:
-                self.supply_list.append({"type": line["type"], "medium": line["medium"], "medium_num": line["medium_num"], "expiration": line["expiration"]})
+                self.supply_list.append({"medium": line["medium"], "medium_num": line["medium_num"], "expiration": line["expiration"]})
 
 
     def get_supply_list(self):
@@ -33,16 +32,18 @@ class Water:
         return round(total)
 
 
-    def remove(self, match, rmv):
+    # I should find a better name for this 'match' variable as I have done elsewhere
+    def remove(self, match, rmv_amt):
         supply_list = self.get_supply_list()
         for line in supply_list:
-            if match['medium'] == line['medium']:
-                if match['medium_num'] == int(line['medium_num']):
-                    if match['expiration'] == line['expiration']:
-                        if int(line["medium_num"]) >= rmv:
-                            line["medium_num"] = int(line["medium_num"]) - rmv
-                            self.rewrite_file(supply_list)
-                            break
+            if (match['medium'] == line['medium']
+                and match['medium_num'] == int(line['medium_num'])
+                and int(line['medium_num']) >= rmv_amt
+                and match['expiration'] == line['expiration']):
+
+                line['medium_num'] = int(line['medium_num']) - rmv_amt
+                self.rewrite_file(supply_list)
+                break
 
 
     ################### THIS DOES NOT ALLOW FOR MULTIPLE DATES THAT ARE THE SAME ######################
@@ -53,19 +54,50 @@ class Water:
         delta_list = []
         for date in exp_list:
             date_object = ((datetime.strptime(date, "%Y-%m-%d")).date())
-            time_delta = (date_object - today).days
-            delta_list.append(time_delta)
+            time_delta_object = (date_object - today).days
+            delta_list.append(time_delta_object)
 
         closest_days = min(delta_list)
-        closest_date_obj = today + timedelta(closest_days)
+        closest_date_object = today + timedelta(closest_days)
 
         # find the line which the date string comes from
         for line in self.get_supply_list():
-            if datetime.strptime(line['expiration'], "%Y-%m-%d").date() == closest_date_obj:
-                return closest_date_obj, line['medium'], line['medium_num'], closest_days
+            if datetime.strptime(line['expiration'], "%Y-%m-%d").date() == closest_date_object:
+                return closest_date_object, line['medium'], line['medium_num'], closest_days
 
-    def rewrite_file(self, new_file):
+    def rewrite_file(self, new_file_list):
         with open(self.filename, 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=["type", "medium", "medium_num", "expiration"])
+            writer = csv.DictWriter(csv_file, fieldnames=["medium", "medium_num", "expiration"])
             writer.writeheader()
-            writer.writerows(new_file)
+            writer.writerows(new_file_list)
+
+class Sanity:
+    def __init__(self):
+        self.MEDIUMS = ["tall_can", "short_can", "water_brick"]
+
+
+    def add_check(self, supply_list):
+        if supply_list['medium'] in self.MEDIUMS:
+            None
+        else:
+            print("Please enter a valid medium")
+            return False
+        if (supply_list['medium_num']).isinteger():
+            None
+        else:
+            print("Please enter a valid integer for amount of added medium.")
+            return False
+        """if supply_list['expiration'] # Is in a valid date format and later than today:
+            None
+        else:
+            print"""
+        return True
+
+    def remove_check(self, remove_line):
+        if remove_line['medium'] in self.MEDIUMS:
+            None
+        else:
+            print("Please enter a valid medium")
+            return False
+
+        return True
